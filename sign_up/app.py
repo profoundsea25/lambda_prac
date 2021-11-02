9d917028dea08b40f32661a8a84df3201780c606
profoundsea25 <profoundsea25@gmail.com>
2021-11-02 20:25:00 +0900

    로그인, 회원가입

--- sign_up/app.py
+++ sign_up/app.py
@@ -1,10 +1,10 @@
 import json
-
 from pymongo import MongoClient
 import boto3
 import hashlib
 from datetime import datetime, timedelta
 import jwt
+import os
 
 
 def get_secret():
@@ -16,17 +16,13 @@ def get_secret():
     get_secret_value_response = client.get_secret_value(
         SecretId='cnvrt-mongoDB'
     )
-    print(1)
     token = get_secret_value_response['SecretString']
-    print(1)
     return eval(token)
 
 
 def db_ops():
     secrets = get_secret()
-    print(1)
     client = MongoClient("mongodb://{0}:{1}@{2}".format(secrets['user'], secrets['password'], secrets['host']))
-    print(2)
     return client
 
 
@@ -34,16 +30,15 @@ def lambda_handler(event, context):
     secrets = get_secret()
     client = db_ops()
     db = client.dbrecipe
-    secret = secrets['secret']
     body = json.loads(event['body'])
-    email = body['email']
-    password = body['password']
-    pw_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'EMAIL': email, 'PASSWORD': pw_hash})
 
-    if event['httpMethod'] == 'OPTIONS':
+    username_receive = body['username_give']
+    email_receive = body['email_give']
+    email_exists = bool(db.users.find_one({"EMAIL": email_receive}))
+
+    if email_exists:
         return {
-            "statusCode": 200,
+            "statusCode": 423,
             'headers': {
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Origin': '*',
@@ -51,35 +46,26 @@ def lambda_handler(event, context):
             },
         }
 
-    if result is not None:
-        _id = str(result['_id'])
-        payload = {
-            'user_id': _id,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-        }
-        token = jwt.encode(payload, secret, "HS256")
-        return {
-            "statusCode": 200,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
-            },
-            "body": json.dumps({
-                "result": "success",
-                "token" : token
-            }),
-        }
-    else:
-        return {
-            "statusCode": 401,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
-            },
-            "body": json.dumps({
-                "result": "fail",
-                "msg": '아이디/비밀번호가 일치하지 않습니다.'
-            }),
-        }
+    password_receive = body['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "USERNAME": username_receive,  # 사용자 이름 / 프로필에 표시되는 이름
+        "EMAIL": email_receive,  # 이메일
+        "PASSWORD": password_hash,  # 비밀번호
+        "PROFILE_PIC": "",  # 프로필 사진 파일 이름
+        "PROFILE_PIC_REAL": f"{os.environ['BUCKET_ENDPOINT']}/profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "PROFILE_INFO": ""  # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+
+    return {
+        "statusCode": 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST'
+        },
+        "body": json.dumps({
+            "result": "success",
+        }),
+    }
